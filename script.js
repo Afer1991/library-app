a8527e18e033a696fd2758f0b109647e52490036
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-05-04 15:44:10 -0400

    Add function to display books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const table = document.getElementById("table");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -11,3 +12,16 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function displayBook() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let row = table.insertRow(i + 1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    cell1.innerHTML = myLibrary[i].title;
+    cell2.innerHTML = myLibrary[i].author;
+    cell3.innerHTML = myLibrary[i].pages;
+    cell4.innerHTML = myLibrary[i].read;
+  }
+}
